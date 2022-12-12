a4d54e77b67c50e18d44d333e170f4f162e4229e
alexandermin <alexander_min24@milton.edu>
2022-12-12 12:05:14 -0500

    Final Submission

--- src/SpiderMan.java
+++ src/SpiderMan.java
@@ -4,7 +4,7 @@ public class SpiderMan {
 
     //VARIABLE DECLARATION SECTION
     //Here's where you state which variables you are going to use.
-    public String name;               //name of the hero
+    public String name;
     public int xpos;                  //the x position
     public int ypos;                  //the y position
     public int dx;                    //the speed of the hero in the x direction
